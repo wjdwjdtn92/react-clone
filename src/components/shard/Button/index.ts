107d78568819a63c3cd4660c0cef53201443503f
Jung Su <wjdwjdtn92@naver.com>
2023-07-14 15:35:54 +0900

    [Feat] Button 컴포넌트 추가

---
+++ src/components/shard/Button/index.ts
@@ -0,0 +1,20 @@
+import { VDomType } from '../../../types';
+
+interface ButtonProps {
+  onClick: () => void;
+  children?: VDomType | string;
+}
+
+function Button({ onClick, children }: ButtonProps): VDomType {
+  return {
+    type: 'button',
+    props: {
+      class:
+        'text-2xl text-neutral-50 border-4 rounded-3xl border-sky-400 p-[16px] bg-sky-400 hover:text-sky-500 hover:bg-neutral-50 ',
+      onClick,
+    },
+    children: [children],
+  };
+}
+
+export default Button;
